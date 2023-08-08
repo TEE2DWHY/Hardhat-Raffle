262e6babe2d371f54f1340011550c378d2313a34
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-08-08 02:34:59 -0700

    created enter raffle event

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -2,27 +2,30 @@
 pragma solidity ^0.8.19;
 error Raffle__NotEnoughtETH();
 
-contract Raffle{
-// State Variales
-uint256 private immutable i_entracefee; // immutable variables are cheap (gas wise)
-address payable[] private s_players; // we added the paybale keyword because one of the address would be the winner and would recieve eth.
+contract Raffle {
+    // State Variales
+    uint256 private immutable i_entracefee; // immutable variables are cheap (gas wise)
+    address payable[] private s_players; // we added the paybale keyword because one of the address would be the winner and would recieve eth.
+    // Events
+    event RaffleEnter(address indexed player); // create an event
 
-  constructor(uint256 entrancfee){
-      i_entracefee = entrancfee;
-  }
-
-function enterRaffle() public payable{
-      if(msg.value < i_entracefee){
-        revert Raffle__NotEnoughtETH();
-      }
-      s_players.push(payable(msg.sender)); // add players to array of players
+    constructor(uint256 entrancfee) {
+        i_entracefee = entrancfee;
     }
 
-function getEntranceFee() public view returns(uint256){
-    return i_entracefee;
-}
+    function enterRaffle() public payable {
+        if (msg.value < i_entracefee) {
+            revert Raffle__NotEnoughtETH();
+        }
+        s_players.push(payable(msg.sender)); // add players to array of players
+        emit RaffleEnter(msg.sender); // emit even with the address of a player (we emit an event when we update a dynamic array or mapping)
+    }
 
-function getPlayers(uint256 index) public view returns(address){
-    return s_players[index];
-}
+    function getEntranceFee() public view returns (uint256) {
+        return i_entracefee;
+    }
+
+    function getPlayers(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
 }
